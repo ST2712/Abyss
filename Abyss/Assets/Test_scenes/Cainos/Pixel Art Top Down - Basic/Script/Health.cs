3fa899864efa6c5fefe9070304acbda0cb312aba
ST2712 <2220211016@estudiantesunibague.edu.co>
2024-04-18 12:58:39 -0500

    Fix: Hearthbit sound
    Bug of sound when only was 1 life left was fixed.

--- Abyss/Assets/Test_scenes/Cainos/Pixel Art Top Down - Basic/Script/Health.cs
+++ Abyss/Assets/Test_scenes/Cainos/Pixel Art Top Down - Basic/Script/Health.cs
@@ -7,42 +7,65 @@ public class Health : MonoBehaviour
 {
     public int health;
     public int numberOfHearts;
-
     public bool extraHealth;
     
     public Image[] hearts;
     public Sprite fullHeart;
     public Sprite emptyHeart;
-
     public Sprite extraHeart;
+
+    private AudioSource heartBite;
+
+    void Start()
+    {
+        heartBite = GetComponent<AudioSource>();
+    }
 
     void Update()
     {
-        if(health > numberOfHearts){
+        if(health > numberOfHearts)
+        {
             health = numberOfHearts;
         }
 
-
-        for (int i = 0; i < hearts.Length; i++){
-            if (i < health){
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
                 hearts[i].sprite = fullHeart;
-            }else{
+            }
+            else
+            {
                 hearts[i].sprite = emptyHeart;
             }
 
-            if (i < numberOfHearts){
-                hearts[i].enabled = true;
-            }else{
-                hearts[i].enabled = false;
-            }
+            hearts[i].enabled = i < numberOfHearts;
         }
 
-        if(extraHealth == true){
+        if(extraHealth)
+        {
             hearts[3].sprite = extraHeart;
             hearts[3].enabled = true;
         }
+
+        if(health == 1)
+        {
+            if (!heartBite.isPlaying)
+            {
+                heartBite.Play();
+                StartCoroutine(PauseSoundAndResume());
+            }
+        }
     }
 
-    
+    IEnumerator PauseSoundAndResume()
+    {
+        yield return new WaitForSeconds(1f);
 
+        if(heartBite.isPlaying)
+        {
+            heartBite.Stop();
+            heartBite.PlayDelayed(1f);
+        }
+    }
 }
