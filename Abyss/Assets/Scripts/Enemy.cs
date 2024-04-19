1836d5c22d0684ad1d64ee31c32eaaad0368d2ee
ST2712 <2220211016@estudiantesunibague.edu.co>
2024-04-19 17:13:13 -0500

    Add: Enemy drop a coin when die

--- Abyss/Assets/Scripts/Enemy.cs
+++ Abyss/Assets/Scripts/Enemy.cs
@@ -8,30 +8,53 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float health;
     private Animator animator;
     private AudioSource audioSource;
+    [SerializeField] private GameObject coinPrefab;
     void Start()
     {
         animator = GetComponent<Animator>();
         audioSource = GetComponent<AudioSource>();
     }
 
-    public void takeDamage(float damage){
+    public void takeDamage(float damage)
+    {
         health -= damage;
-        if(health <= 0){
-            audioSource.Play();
-            die();
-            this.enabled = false;
-            GetComponent<Collider2D>().enabled = false;
-            Destroy(gameObject, 4);
+        if (health <= 0)
+        {
+            StartCoroutine(dieWithDelay());
         }
     }
 
-    private void die(){
+    private IEnumerator dieWithDelay()
+    {
+        audioSource.Play();
+        die();
+        this.enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+        Destroy(gameObject, 4);
+        yield return new WaitForSeconds(3.9f);
+        dropCoin();
+    }
+
+    private void die()
+    {
         animator.SetTrigger("Dead");
+    }
+
+    private void dropCoin()
+    {
+        if (coinPrefab != null)
+        {
+            Instantiate(coinPrefab, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Debug.LogWarning("Prefab de moneda no asignado en el script Enemy.");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
