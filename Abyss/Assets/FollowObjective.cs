1f8f69ed04830a3907a06e97977fdbae1ca3f3d5
ST2712 <2220211016@estudiantesunibague.edu.co>
2024-04-21 10:13:33 -0500

    Add: Main is chased by Enemy
    In test scenes, now, Enemy can chase main character

---
+++ Abyss/Assets/FollowObjective.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowObjective : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private NavMeshAgent navMeshAgent;
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.updateRotation = false;
+        navMeshAgent.updateUpAxis = false;
+    }
+
+    private void Update()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+}
